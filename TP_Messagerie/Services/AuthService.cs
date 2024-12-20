88d426e3a1596618559d5646fc09842eafe81da0
Faxiis <antho.lavoute@gmail.com>
2024-12-20 14:17:59 +0100

    AuthService Added

---
+++ TP_Messagerie/Services/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Driver;
+using TP_Messagerie.Data;
+
+namespace TP_Messagerie.Services
+{
+    public class AuthService
+    {
+        private readonly UserService _userService;
+        public User? CurrentUser { get; set; }
+        public bool IsAuthenticated => CurrentUser != null;
+
+        public AuthService(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<User?> LoginAsync(string username, string password)
+        {
+            var user = await _userService.LoginAsync(username, password);
+            if (user != null)
+            {
+                CurrentUser = user; 
+            }
+            return user;
+        }
+
+        public async Task<bool> RegisterAsync(User user)
+        {
+            var existingUser = await _userService.GetUserByIdAsync(user.Id);
+            if (existingUser != null)
+            {
+                return false;
+            }
+
+            await _userService.CreateUserAsync(user);
+            return true;
+        }
+    }
+}
--- TP_Messagerie/obj/Debug/net8.0/TP_Messagerie.AssemblyInfo.cs
+++ TP_Messagerie/obj/Debug/net8.0/TP_Messagerie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064e0fff0167fc4555b16337e1186153eff59654")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797afd9321c3cf001287fb52c5c9005e99b50117")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
