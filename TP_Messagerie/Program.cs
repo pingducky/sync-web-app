b7e1c027bf3add37b27a5bbe4c35c1a04a15dbbc
Hugo <ducky4tools@gmail.com>
2024-12-20 11:18:34 +0100

    Setup mudBlazor + création .gitignore

--- TP_Messagerie/Program.cs
+++ TP_Messagerie/Program.cs
@@ -1,8 +1,11 @@
 using TP_Messagerie.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMudServices();
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
--- TP_Messagerie/obj/Debug/net8.0/TP_Messagerie.AssemblyInfo.cs
+++ TP_Messagerie/obj/Debug/net8.0/TP_Messagerie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c035e72cd157eb141e1f1d38448ad86a472f83")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
