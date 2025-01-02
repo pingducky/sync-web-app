ca6b71b5b44e168cd67d3908cbef48e0373b6da5
Hugo <ducky4tools@gmail.com>
2025-01-02 21:03:06 +0100

    Création et implémentation du composant pour connaitre le statut de la connexion internet.

--- TP_Messagerie/obj/Debug/net8.0/TP_Messagerie.AssemblyInfo.cs
+++ TP_Messagerie/obj/Debug/net8.0/TP_Messagerie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22165b346b0f835bd8ac800732145288028cf108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f7ba899c7dce1ec5f89f03e1deaef86ce3bd24")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
