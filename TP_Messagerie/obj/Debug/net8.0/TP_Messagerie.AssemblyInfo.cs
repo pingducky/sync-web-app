8e28809debec2b3459620d233e3c7ec44d07c1b6
Hugo <ducky4tools@gmail.com>
2025-01-07 21:55:44 +0100

    Supression d'un composant de test.

--- TP_Messagerie/obj/Debug/net8.0/TP_Messagerie.AssemblyInfo.cs
+++ TP_Messagerie/obj/Debug/net8.0/TP_Messagerie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56417f30420a1d922bf6ccd64a3665eda1d38571")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45294f3180070e391de30fd9590df32188317a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_Messagerie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
